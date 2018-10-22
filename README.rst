24aae698a7d9984ca907135959d6b9b1712216ff
tunnelshade <tunnelshade@gmail.com>
2018-10-22 11:59:20 +0200

    Updated readme and help

--- README.rst
+++ README.rst
@@ -22,13 +22,15 @@ Hosts
 Targets
 =======
 
-Non sudo privileged access is required with ptrace_scope set to 0.
+Non sudo privileged access is required with ptrace_scope set to 0 for stracing.
 
 Walkthrough
 ***********
 
-Modules
-*******
+TBD
+
+Utilities
+*********
 
 1. Network Routing
 ==================
@@ -46,17 +48,38 @@ Prerequisites
    # Load iptables module, if not already
    $> modprobe ip_tables
 
+Usage
+^^^^^
+
+*INCOMING = PREROUTING dnat*
+*OUTGOING = OUTPUT dnat*
+
+To redirect incoming traffic from target to your reverse proxy
+
+.. code-block::
+
+   ./bin/rinnegan.sh "<filter>" agent iptables incoming add <some_ip_of_target> <port> <reverse_proxy_ip:reverse_proxy_port>
+
+To delete a rule, just replace ``add`` with ``remove``
+
+.. code-block::
+
+   ./bin/rinnegan.sh "<filter>" agent iptables incoming remove <some_ip_of_target> <port> <reverse_proxy_ip:reverse_proxy_port>
+
+Let us say you want to intercept traffic arriving at a web server, then redirecting incoming traffic on that web server target makes sense.
+One caveat of this is all clients connecting to this server will get their traffic rerouted. Now what if you want to intercept traffic for
+one particular client. This is where redirect outgoing on client instance makes sense.
+
+.. code-block::
+
+   ./bin/rinnegan.sh "<filter>" agent iptables outgoing add <ip_of_server> <port_of_server> <reverse_proxy_ip:reverse_proxy_port>
+
+Just replace ``add`` with ``remove`` to remove this redirection.
+
 
 Known Issues
 ************
 
 This is quite unstable, so expect a bumpy ride
-
-* ps on alpine is from busybox, so to get the real deal install procps
-  on all alpine containers.
-
-.. code-block::
-
-   ./bin/rinnegan.sh "." exec apk add --no-cache procps
 
 * Agent just smoked up too many processes, what to do? Just restart your containers/vms.
--- agent/cmd/module.go
+++ agent/cmd/module.go
@@ -21,7 +21,7 @@ var moduleCmd = &cobra.Command{
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List modules",
-	Long:  "List rinnegan modules",
+	Long:  "List currently running rinnegan modules",
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Info("Listing all current modules")
 		log.Info(daemon.HTTPGet("/module"))
@@ -31,7 +31,7 @@ var listCmd = &cobra.Command{
 var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Run modules",
-	Long:  "Run rinnegan modules",
+	Long:  "Run rinnegan module with specified type and arguments",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Info("Running a module")
@@ -41,7 +41,7 @@ var runCmd = &cobra.Command{
 var stopCmd = &cobra.Command{
 	Use:   "stop",
 	Short: "Stop module",
-	Long:  "Stop rinnegan modules",
+	Long:  "Stop rinnegan module by name which can be obtained by running list",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Info("Switching to module operations")
@@ -51,8 +51,8 @@ var stopCmd = &cobra.Command{
 
 var psCmd = &cobra.Command{
 	Use:   "ps",
-	Short: "Ps module",
-	Long:  "Ps module",
+	Short: "Collects all running processes along with sockets opened for listening",
+	Long:  "Collects all running processes along with sockets opened for listening",
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Info("Running ps module")
 		log.Info(daemon.HTTPPost("/module/ps", url.Values{}))
@@ -62,7 +62,7 @@ var psCmd = &cobra.Command{
 var straceCmd = &cobra.Command{
 	Use:   "strace PID TRACE_TYPE",
 	Short: "strace module",
-	Long:  "strace module, TRACE_TYPE is directly passed to strace -e option",
+	Long:  "strace module that captures syscalls for a pid, TRACE_TYPE is directly passed to strace -e option",
 	Args:  cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Info("Running strace module")
@@ -78,8 +78,8 @@ var straceCmd = &cobra.Command{
 
 var fridaCmd = &cobra.Command{
 	Use:   "frida PID SCRIPT_NAME",
-	Short: "frida module",
-	Long:  "frida module",
+	Short: "frida module used to run various scripts",
+	Long:  "frida module used to run various scripts. For list of available scripts have a look at build/frida",
 	Args:  cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Info("Running frida module")
