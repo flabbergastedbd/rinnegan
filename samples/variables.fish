9e5bd16fd982dcc8f17b148d33293b6e7b9079f2
tunnelshade <tunnelshade@gmail.com>
2019-01-29 20:21:55 +0530

    Updated samples

--- samples/variables.fish
+++ samples/variables.fish
@@ -1,4 +1,12 @@
+# Path to hosts file, can be relative or absolute
 set -gx RINNEGAN_HOSTS_FILE ./samples/hosts
+
+# Ensure appropriate hostname so that agents on targets can reach this to push data
+# Eg: If using docker containers, add an entry in host's /etc/hosts that redirects
+# this domain to localhost as containers pick up dns resolution on host generally.
 set -gx RINNEGAN_INFLUX_HOST http://influxdb:8086
+
+# Set to `true` when playing with docker containers
 set -gx RINNEGAN_DOCKER false
+
 set -gx RINNEGAN_DEBUG false
